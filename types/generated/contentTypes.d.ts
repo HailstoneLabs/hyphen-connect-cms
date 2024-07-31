cfe8a3b9d289f0626cfa65607b903ac44eb254ac
ryan-hailstonelabs <ryan.prjsl@gmail.com>
2024-07-31 16:56:30 +0700

    feat: update article schema

--- types/generated/contentTypes.d.ts
+++ types/generated/contentTypes.d.ts
@@ -793,7 +793,7 @@ export interface ApiArticleArticle extends Schema.CollectionType {
   info: {
     singularName: 'article';
     pluralName: 'articles';
-    displayName: 'Blog';
+    displayName: 'Article';
     description: 'Create your blog content';
   };
   options: {
